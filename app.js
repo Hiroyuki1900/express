48ebd5ae879a9ef4ec7367617d5b843fa58790b5
Hiroyuki1900 <2201355009xg@cyber-u.ac.jp>
2024-11-20 19:44:42 +0900

    add hello page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var hellowRouter = require('./routes/users');
+var helloRouter = require('./routes/users');
 
 var app = express();
 
